32e0ef953f76dba6ee4d6e461e0ee810008f29a7
danielk <daniel.kastinen@irf.se>
2022-10-08 18:49:07 +0200

    update model and verify vs paper

--- dasst/ejection_models/comets/sublimation.py
+++ dasst/ejection_models/comets/sublimation.py
@@ -2,22 +2,41 @@ import numpy as np
 import scipy.constants as const
 
 
-def whipple_1951_dMdt(body_radius, helio_distance, solar_luminosity = 4e26, body_H_W = 1.88e6):
-    return np.pi*body_radius**2*solar_luminosity/(4*np.pi*helio_distance**2)/body_H_W
+def whipple_1951_dMdt(
+                helio_distance, 
+                body_radius, 
+                albedo = 0.2, 
+                solar_luminosity = 4e26, 
+                sublimation_heat = 1.88e6,
+            ):
+    S_flux = solar_luminosity/(4*np.pi*helio_distance**2)
+    S_input = np.pi*body_radius**2*S_flux
+    return S_input*(1 - albedo)/sublimation_heat
 
 
 def whipple_1951_vel(
-                helio_distance, body_radius, bulk_density, mass, body_mass, dMdt,
-                tau_Wip = 0.25,
-                mu_Wip = 20*1.661e-24*1e-3, T_g0 = 300, K_drag = 26.0/9.0,
+                helio_distance, 
+                body_radius,
+                body_mass,
+                particle_bulk_density,
+                particle_mass,
+                dMdt,
+                gas_molecule_mass = 20*1.661e-24*1e-3, 
+                surface_temperature_coeff = 300, 
+                K_drag = 26.0/9.0,
             ):
+    r = helio_distance/const.au
+    T = surface_temperature_coeff/np.sqrt(r)
+    v_g = np.sqrt(8*const.k*T/(np.pi*gas_molecule_mass))
 
-    v_g = np.sqrt(8*const.k*T_g0/(np.pi*mu_Wip))/(helio_distance/const.au)**tau_Wip
-    v2 = dMdt*K_drag*v_g/(2*np.pi*body_radius)
-    s = np.cbrt(mass*3/(4*np.pi*bulk_density))
-    grav_effect = 2*const.G*(body_mass)/body_radius
+    particle_radius = np.cbrt(particle_mass*3/(4*np.pi*particle_bulk_density))
+    A = np.pi*particle_radius**2
+    A_to_m = A/particle_mass
+    
+    drag_v = (K_drag*0.5)*(dMdt*v_g/(np.pi*body_radius))*A_to_m
+    grav_effect = 2*const.G*body_mass/body_radius
 
-    vel_mag = v2/(s*bulk_density)*(3.0/4.0) - grav_effect
+    vel_mag = drag_v - grav_effect
     vel_mag = np.sqrt(vel_mag) 
 
     return vel_mag
