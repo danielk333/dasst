bbfb5c900560702f7b6f608503ee14f9ed268ce5
danielk <daniel.kastinen@irf.se>
2024-01-16 15:00:08 +0100

    formatting

--- examples/d_criteria.py
+++ examples/d_criteria.py
@@ -1,55 +1,43 @@
-'''
+"""
 D criterion example
 =========================
 
 Docstring for this example
-'''
+"""
 from numpy import radians as rad
 import dasst.functions.d_criteria as dlib
 import pyorb
 
-G_au = pyorb.get_G(length='AU', mass='kg', time='s')
+G_au = pyorb.get_G(length="AU", mass="kg", time="s")
 
 kw = dict(
-    M0=pyorb.M_sol, 
+    M0=pyorb.M_sol,
     G=G_au,
-    direct_update=True, 
-    auto_update=True, 
-    degrees=False, 
+    direct_update=True,
+    auto_update=True,
+    degrees=False,
 )
 
 orb_a = pyorb.Orbit(
-    a=1.11, 
-    e=0.6, 
-    i=rad(68), 
-    omega=rad(11), 
-    Omega=rad(5), 
-    anom=rad(10.01),
-    **kw
+    a=1.11, e=0.6, i=rad(68), omega=rad(11), Omega=rad(5), anom=rad(10.01), **kw
 )
-print('Orbit A:')
+print("Orbit A:")
 print(orb_a)
 
 orb_b = pyorb.Orbit(
-    a=1.1, 
-    e=0.5, 
-    i=rad(67), 
-    omega=rad(12), 
-    Omega=rad(5), 
-    anom=rad(10),
-    **kw
+    a=1.1, e=0.5, i=rad(67), omega=rad(12), Omega=rad(5), anom=rad(10), **kw
 )
-print('Orbit b:')
+print("Orbit b:")
 print(orb_b)
 
-print(f'D_SH(A,B)')
+print("D_SH(A,B)")
 print(dlib.D_SH(orb_a, orb_b))
 
-print(f'D_V(A,B)')
+print("D_V(A,B)")
 print(dlib.D_V(orb_a, orb_b))
 
-print(f'D_SH(A,A)')
+print("D_SH(A,A)")
 print(dlib.D_SH(orb_a, orb_a))
 
-print(f'D_V(A,A)')
+print("D_V(A,A)")
 print(dlib.D_V(orb_a, orb_a))
