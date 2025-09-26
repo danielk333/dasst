78b94281af50289a029b9efe8cd3304096b08e3c
danielk <daniel.kastinen@irf.se>
2025-09-26 10:02:16 +0200

    added kernel example

---
+++ examples/loading_kernels.py
@@ -0,0 +1,13 @@
+import argparse
+from astropy.time import Time
+from pathlib import Path
+import dasst
+
+parser = argparse.ArgumentParser()
+parser.add_argument("kernel_path", type=Path, help="Path to the spice kernel")
+args = parser.parse_args()
+
+epoch = Time("2024-01-01T00:00:00", format="isot", scale="utc")
+states = dasst.frames.get_solarsystem_body_states(["Sun", "Earth"], epoch, args.kernel_path)
+
+print(states)
