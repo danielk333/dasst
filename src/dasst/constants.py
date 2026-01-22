2290976baf30f37e004a2d5c7e8a7f3ce8a0d964
EndPi <97395521+EndPi@users.noreply.github.com>
2026-01-22 10:02:44 +0100

    Test push to see if this works

--- src/dasst/constants.py
+++ src/dasst/constants.py
@@ -20,3 +20,17 @@ L_S = 2.833e6  # J/kg
 
 #todo citation?
 """
+
+AU: float = 1.495978707e11 # astronomical unit [m]
+
+# Time
+DAY: float = 86400.0 # seconds in a day
+YEAR: float = 365.25 * DAY # Julian year [s]
+
+# Gravitational parameters
+MU_SUN: float = 1.32712440018e20 # GM_sun [m^3/s^2]
+MU_EARTH: float = 3.986004418e14 # GM_earth [m^3/s^2]
+
+# Radii
+R_SUN: float = 6.957e8 # [m]
+R_EARTH: float = 6.371e6 # [m]
